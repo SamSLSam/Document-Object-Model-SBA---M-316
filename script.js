78e34805c91f6026e0e1b6953c9003be07ad8feb
SamSLSam <info.snnl@gmail.com>
2025-05-23 22:03:07 -0400

    5

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 //  Cache elements using getElementById and querySelector
 const form = document.getElementById("name").form;
 const messageBox = document.querySelector("#message");
+
+// Create a new paragraph element
+const newPara = document.createElement("p") ;
+newPara.textContent = "Click a menu item to see more!";
+newPara.style.color = "lightgreen";
