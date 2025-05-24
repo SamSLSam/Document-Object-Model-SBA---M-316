d98f495e26413c314d936d9a5ab0adfe84f87920
SamSLSam <info.snnl@gmail.com>
2025-05-23 22:58:54 -0400

    8

--- script.js
+++ script.js
@@ -13,8 +13,16 @@ document.body.appendChild(newPara);
 // Adding Event listener for input validation & Browser Object Model: preventDefault()
 form.addEventlistner("Submit", function (event) {
     const nameInput = document.getElementById("name").value; 
-    If (nameInput.lenght < 3) {
+    If (nameInput.length < 3) {
         alert("Name must be at least 3 characters long");
         event.preventDefault();
+    }
+});
+
+// On interaction messageBox Modififcation on Form input changes trigger border and placeholder update 
+messageBox.addEventListener("input", function(){
+    if (messageBox.value.length > 0) {
+        messageBox.style.border = "2px solid green";
+        messageBox.setAttribute("placeholder", "Keep Trying...");
     }
 });
