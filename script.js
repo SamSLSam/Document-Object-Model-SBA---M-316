de94063e210bf246caefdc4814004d7cf1c64abf
SamSLSam <info.snnl@gmail.com>
2025-05-23 22:26:20 -0400

    6

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 const form = document.getElementById("name").form;
 const messageBox = document.querySelector("#message");
 
-// Create a new paragraph element
+// Create a new paragraph element Modify element using textContent & style
 const newPara = document.createElement("p") ;
 newPara.textContent = "Click a menu item to see more!";
 newPara.style.color = "lightgreen";
+
+// Using appendChild to add (newPara) to body
+document.body.appendChild(newPara);
+
+// Adding Event listener for input validation
+
